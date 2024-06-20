a686bd1a4676766e91522ddd5230d8bbe9b613da
Steve Leach <steve.leach@yahoo.com>
2024-06-20 17:10:48 +0100

    Added home page test scenario

--- cmd/httptest/steps.go
+++ cmd/httptest/steps.go
@@ -9,13 +9,22 @@ import (
 )
 
 func addFlashcardSteps(suite *test.TestSuite) {
+	suite.AddStep("the home page is accessed", whenHomePageAccessed)
 	suite.AddStep("the test deck is accessed", whenTestDeckAccessed)
+	suite.AddStep("the page contains `text`", thenPageContains)
 	suite.AddStep("the deck has the question `question`", thenDeckHasQuestion)
 }
 
+func whenHomePageAccessed(data *test.StepData) {
+	openPage("/", data)
+}
+
 func whenTestDeckAccessed(data *test.StepData) {
-	url := data.Suite.BaseUrl + "/deck/TEST-CODE"
-	response, err := http.Get(url)
+	openPage("/deck/TEST-CODE", data)
+}
+
+func openPage(url string, data *test.StepData) {
+	response, err := http.Get(data.Suite.BaseUrl + url)
 
 	if err == nil {
 		if response.StatusCode < 400 {
@@ -44,5 +53,18 @@ func thenDeckHasQuestion(data *test.StepData) {
 	}
 	if !found {
 		data.Suite.ReportError("Did not find question %s", data.Text)
+	}
+}
+
+func thenPageContains(data *test.StepData) {
+	if data.Suite.CurrentPageDoc == nil {
+		data.Suite.ReportError("No current page document")
+		return
+	}
+
+	pageContent := data.Suite.CurrentPageDoc.Text()
+
+	if !strings.Contains(pageContent, data.Step.Values[0]) {
+		data.Suite.ReportError("Page does not contain text: %s", data.Step.Values[0])
 	}
 }
--- test/scripts/main.feature
+++ test/scripts/main.feature
@@ -1,6 +1,9 @@
 # Comments are ignored 
 
-Scenario: Test scenario 1
+Scenario: Home page accessed
+    When the home page is accessed
+    Then the page contains `Learning with Flashcards.`
 
+Scenario: Test deck accessed
     When the test deck is accessed
 	Then the deck has the question `What is the meaning of life?`
